02b5d27905075508f64f2f119a3654745771698e
Jonas Nielsen <dev@jonas92.com>
2016-08-10 16:13:22 +0200

    Register MailServiceProvider and add configurations

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -81,6 +81,9 @@ $app->singleton(
 // $app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
+$app->register('Illuminate\Mail\MailServiceProvider::class');
+$app->configure('mail');
+$app->configure('services');
 
 /*
 |--------------------------------------------------------------------------

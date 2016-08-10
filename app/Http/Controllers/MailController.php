eebd9f468e6b66fc1f49b06a43026f460ca2fed1
Jonas Nielsen <dev@jonas92.com>
2016-08-10 16:36:25 +0200

    Add MailController

---
+++ app/Http/Controllers/MailController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Mail;
+
+class MailController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    function send() {
+        $username = 'Jonas';
+        $email = 'master@jonas92.com';
+
+        Mail::send('email/verify_email', [
+        'username' => $username
+        ], function ($message) use ($email) {
+            $message->from('mailbot@website.com', 'Website');
+            $message->to($email);
+            $message->subject('Welcome to Website');
+        });
+
+        return response('Welcome mail send to ' . $email, 200);
+    }
+}

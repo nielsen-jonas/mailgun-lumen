a9cfffb5ad89a6a3391e6edf2a5e34b8ac132256
Jonas Nielsen <dev@jonas92.com>
2016-08-10 16:40:25 +0200

    Add route to MailController

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,7 @@
 $app->get('/', function () use ($app) {
     return $app->version();
 });
+
+$app->group(['prefix' => 'mail', 'namespace' => 'App\Http\Controllers'], function () use ($app) {
+	$app->get('send', 'MailController@send');
+});
